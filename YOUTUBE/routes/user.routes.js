9c24a4362e2e1be44d381fb5f5f93e780f439c8e
irfanbaig7 <ibaig9356@gmail.com>
2025-07-16 21:15:49 +0530

    create our user and uploding req and data from postman successfully, and showing into our DB 😉

--- YOUTUBE/routes/user.routes.js
+++ YOUTUBE/routes/user.routes.js
@@ -1,8 +1,31 @@
 import express from "express";
+import bcrypt from "bcrypt"
+import mongoose from "mongoose";
+import userModel from "../models/userSchema.js";
+import cloudinary from "../config/cloudinary.js";
 const router = express.Router();
 
-router.post("/signup", (req, res) => {
-    res.send("All good😊 chal gaya Oyee")
+router.post("/signup", async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        const uploadeImg = await cloudinary.uploader.upload(req.files.logoUrl.tempFilePath)
+
+        const newUser = new userModel({
+            _id: new mongoose.Types.ObjectId,
+            channelName: req.body.channelName,
+            phone: req.body.phone,
+            email: req.body.email,
+            password: hashedPassword,
+            logoUrl: uploadeImg.secure_url,
+            logoId: uploadeImg.public_id
+        })
+        let user = await newUser.save()
+        res.status(202).json({
+            user
+        })
+    } catch (error) {
+        console.log(error.message);
+    }
 })
 
 export default router
