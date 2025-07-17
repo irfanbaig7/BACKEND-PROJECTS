e378ee4e0a3175854f2ed35590ff1df4ced043dc
irfanbaig7 <ibaig9356@gmail.com>
2025-07-17 06:30:14 +0530

    create login req, JWT token, pass data with status json or more Logic

--- YOUTUBE/routes/user.routes.js
+++ YOUTUBE/routes/user.routes.js
@@ -1,8 +1,12 @@
 import express from "express";
 import bcrypt from "bcrypt"
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken"
+
+
 import userModel from "../models/userSchema.js";
 import cloudinary from "../config/cloudinary.js";
+
 const router = express.Router();
 
 router.post("/signup", async (req, res) => {
@@ -25,8 +29,49 @@ router.post("/signup", async (req, res) => {
         })
     } catch (error) {
         console.log(error);
-        res.status(500).json({error: "Somthing went wrong", message : error.message})
+        res.status(500).json({ error: "Somthing went wrong", message: error.message })
     }
-}) 
+})
+
+router.post("/login", async (req, res) => {
+    try {
+        const existingUser = await userModel.findOne({ email: req.body.email })
+        if (!existingUser) {
+            return res.status(404).json({ message: "User not Found 😒" })
+        }
+
+        // Now compairing Password
+        const inValid = await bcrypt.compare(req.body.password, existingUser.password)
+
+        if (!inValid) {
+            return res.status(404).json({ message: "Invalid credentials" })
+        }
+
+        const token = jwt.sign({
+            _id: existingUser._id,
+            channelName: existingUser.channelName,
+            email: existingUser.email,
+            phone: existingUser.phone,
+            logoId: existingUser.logoId,
+        }, process.env.JWT_TOKEN, { expiresIn: "10d" })
+
+
+        res.status(200).json({
+            _id: existingUser._id,
+            channelName: existingUser.channelName,
+            email: existingUser.email,
+            phone: existingUser.phone,
+            logoId: existingUser.logoId,
+            logoUrl: existingUser.logoUrl,
+            token: token,
+            subscribers: existingUser.subscribers,
+            subscribedChannels: existingUser.subscribedChannels
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "Somthing went wrong", message: error.message })
+    }
+})
 
 export default router
